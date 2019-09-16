c55f9d3a86058af331181a1c57d66e6456b55968
Shi Yan <xjtuyanshi@gmail.com>
2019-09-15 17:48:23 -0700

    fixed another typo of waypoint -:(

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -91,7 +91,7 @@ class WaypointUpdater(object):
     def waypoints_cb(self, waypoints):
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
-            self.waypoints_2d =[[waypoint.pose.pose.position.x, Waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoints_2d =[[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             # add waypoints to KD tree for getting closest waypoint purpose
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
